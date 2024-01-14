0072a62afc1edb5dba472325e38311f911057ee0
MericaTudias <mat6524@studets.uc-bcf.edu.ph>
2024-01-15 03:31:33 +0800

    ThirdCommit

--- PHP-OperatorTypes.php
+++ PHP-OperatorTypes.php
@@ -13,18 +13,25 @@
         $b = 10;
 
         $c = $a + $b;
+        print("$a + $b = $c <br>");
         echo "Addition Operation Result: $c <br/>";
         $c = $a - $b;
+        print("$a - $b = $c <br>");
         echo "Subtraction Operation Result: $c <br/>";
         $c = $a * $b;
+        print("$a * $b = $c <br>");
         echo "Multiplication Operation Result: $c <br/>";
         $c = $a / $b;
+        print("$a / $b = $c <br>");
         echo "Division Opreration Result: $c <br/>";
         $c = $a % $b;
+        print("$a % $b = $c <br>");
         echo "Modulus Operation Result: $c <br/>";
         $c = $a++;
+        print("$a++ = $c <br>");
         echo "Increment Operation Result: $c <br/>";
-        $c = $a++;
+        $c = $a--;
+        print("$a-- = $c <br>");
         echo "Decrement Operation Result: $c <br/>";
     ?>
 </body>
