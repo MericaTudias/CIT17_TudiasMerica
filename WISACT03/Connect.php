22f404f143baeda8dd26a28db39138a94ad0e378
MericaTudias <mat6524@studets.uc-bcf.edu.ph>
2024-01-17 02:55:17 +0800

    NinthCommit

---
+++ WISACT03/Connect.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "StudentInfo";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+?>
---
+++ WISACT03/WISACT03.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE StudentInfo;
+USE StudentInfo;
+
+CREATE table Users(
+    ID int(255),
+    Username varchar(255),
+    Email varchar(255),
+    PRIMARY KEY(ID)
+);
