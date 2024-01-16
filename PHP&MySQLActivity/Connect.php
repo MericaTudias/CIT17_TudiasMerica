d36d64d6e359acc53b892718449e8e290bd721ef
MericaTudias <mat6524@studets.uc-bcf.edu.ph>
2024-01-17 02:27:07 +0800

    EightCommit

--- PHP&MySQLActivity/Connect.php
+++ PHP&MySQLActivity/Connect.php
@@ -16,15 +16,57 @@ $result = $conn->query($sql);
 
 if ($result) {
     while ($row = $result->fetch_assoc()) {
-        echo "Student ID: " . $row["StudentId"]. "<br>"
+        echo "Student ID: " . $row["StudentID"]. "<br>"
         . "First Name: " . $row["FirstName"]. "<br>"
         . "Last Name: " . $row["FirstLast"]. "<br>"
         . "Date of Birth: " . $row["DateOfBirth"]. "<br>"
         . "Email: " . $row["Email"]. "<br>"
-        . "Phone: " . $row["Phone"]. "<br>"
+        . "Phone: " . $row["Phone"]. "<br>";
     }
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
+$sql = "select * from Course";
+$result = $conn->query($sql);
+
+if ($result) {
+    while ($row = $result->fetch_assoc()) {
+        echo "Course ID: " . $row["CourseID"]. "<br>"
+        . "Course Name: " . $row["CourseName"]. "<br>"
+        . "Credits: " . $row["Credits"]. "<br>";
+    }
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$sql = "select * from Instructor";
+$result = $conn->query($sql);
+
+if ($result) {
+    while ($row = $result->fetch_assoc()) {
+        echo "Instructor ID: " . $row["InstructorID"]. "<br>"
+        . "First Name: " . $row["FirstName"]. "<br>"
+        . "Last Name: " . $row["FirstLast"]. "<br>"
+        . "Email: " . $row["Email"]. "<br>"
+        . "Phone: " . $row["Phone"]. "<br>";
+    }
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$sql = "select * from Enrollment";
+$result = $conn->query($sql);
+
+if ($result) {
+    while ($row = $result->fetch_assoc()) {
+        echo "Enrollment ID: " . $row["EnrollmentID"]. "<br>"
+        . "Student ID: " . $row["StudentID"]. "<br>"
+        . "Course ID: " . $row["CourseID"]. "<br>"
+        . "Enrollment Date: " . $row["EnrollmentDate"]. "<br>"
+        . "Grade: " . $row["Grade"]. "<br>";
+    }
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 ?>
--- PHP&MySQLActivity/PHPScriptDemo.sql
+++ PHP&MySQLActivity/PHPScriptDemo.sql
@@ -6,7 +6,7 @@ CREATE table Student(
     LastName varchar(255),
     DateOfBirth varchar(255),
     Email varchar(255),
-    Phone varchar(255),
+    Phone int(11),
     PRIMARY KEY(StudentID)
 );
 
@@ -22,7 +22,7 @@ CREATE table Instructor(
     FirstName varchar(255),
     LastName varchar(255),
     Email varchar(255),
-    Phone varchar(255),
+    Phone int(11),
     PRIMARY KEY(InstructorID)
 );
 
@@ -31,8 +31,20 @@ CREATE table Enrollment(
     StudentID int(255),
     CourseID int(255),
     ErollmentDate varchar(255),
-    Grade varchar(255),
+    Grade int(255),
     PRIMARY KEY(EnrollmentID),
     FOREIGN KEY(StudentID) REFERENCES Student(StudentID),
     FOREIGN KEY(CourseID) REFERENCES Course(CourseID)
 );
+
+INSERT INTO Student(StudentID, FirstName, LastName, DateOfBirth, Email, Phone)
+VALUES(011, "Jane", "Hopper", "05-25-1971", "eleven@gmail.com", 09011011011);
+
+INSERT INTO Course(CourseID, CourseName, Credits)
+VALUES(01, "CIT17", "1");
+
+INSERT INTO Instructor(InstructorID, FirstName, LastName, Email, Phone)
+VALUES(123, "Martin", "Brenner", "brenner@gmail.com", 09123456789);
+
+INSERT INTO Enrollment(EnrollmentID, StudentID, CourseID, ErollmentDate, Grade)
+VALUES(0101, 011, 01, "10-13-1989", 99);
