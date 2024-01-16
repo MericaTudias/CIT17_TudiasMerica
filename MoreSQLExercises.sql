b1de8049aee1bc78d3f5f8f19819a3a99d7b7e1f
MericaTudias <mat6524@studets.uc-bcf.edu.ph>
2024-01-16 20:02:23 +0800

    SixthCommit

--- MoreSQLExercises.sql
+++ MoreSQLExercises.sql
@@ -16,3 +16,24 @@ CREATE table Course(
     Credits varchar(255),
     PRIMARY KEY(CourseID)
 );
+
+CREATE table Instructor(
+    InstructorID int not null,
+    FirstName varchar(255),
+    LastName varchar(255),
+    Email varchar(255),
+    Phone varchar(255),
+    PRIMARY KEY(InstructorID)
+);
+
+CREATE table Enrollment(
+    EnrollmentID int NOT NULL,
+    StudentID int(255),
+    CourseID int(255),
+    CourseID int(255),
+    ErollmentDate varchar(255),
+    Grade varchar(255),
+    PRIMARY KEY(Enrollment),
+    FOREIGN KEY(StudentID) REFERENCES Student(StudentID),
+    FOREIGN KEY(CourseID) REFERENCES Course(CourseID)
+);
